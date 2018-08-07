9689478c4d2e007595de023167b43058c0bd593e
gm <gleb.mihalkov.work@yandex.ru>
2018-08-07 15:55:53 +0300

    Removing unused code

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -26,16 +26,6 @@ export default class App extends React.Component {
     super(...args);
 
     /**
-     * Default state.
-     * @type {Object}
-     */
-    this.state = {
-      backLink: null,
-      nextLink: null,
-      flip: null
-    };
-
-    /**
      * Collection of routes.
      * @type {Object}
      */
@@ -61,57 +51,11 @@ export default class App extends React.Component {
   }
 
   /**
-   * Returns true if component should be updated.
-   * @param  {Object}  props Next properties.
-   * @param  {Object}  state Next state.
-   * @return {Boolean}       True or false.
-   */
-  shouldComponentUpdate(props, state) {
-    return this.props.location.pathname != props.location.pathname
-      || this.state.backLink != state.backLink
-      || this.state.nextLink != state.nextLink;
-  }
-
-  /**
-   * Returns base part of current location.
-   * @param  {Object} model Current model.
-   * @return {String}       Base part of link.
-   */
-  getBaseLink(model) {
-    let regexp = new RegExp(`/${model.id}$`);
-    let path = this.props.location.pathname;
-    let base = path.replace(regexp, '').replace(/\/$/, '');
-    return base;
-  }
-
-  /**
-   * Handlers models loading.
-   * @param  {Object} model Current model.
-   * @param  {Object} back  Previous model.
-   * @param  {Object} next  Next model.
-   * @return {void}
-   */
-  onModels = (model, back, next) => {
-    let base = this.getBaseLink(model);
-
-    let backLink = back ? `${base}/${back.id}` : null;
-    let nextLink = next ? `${base}/${next.id}` : null;
-
-    this.setState({
-      backLink,
-      nextLink
-    });
-  }
-
-  /**
    * Renders component.
    * @return {*}
    */
   render() {
     let location = this.props.location;
-
-    let backLink = this.state.backLink;
-    let nextLink = this.state.nextLink;
 
     let flip = this.flip;
     let list = this.list;
