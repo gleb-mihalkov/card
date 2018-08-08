c758c7fb8a6da119d011187e48e021e3eca8b102
gm <gleb.mihalkov.work@yandex.ru>
2018-08-08 15:04:44 +0300

    Adding FlipType enum

---
+++ src/App/Flip/FlipType.jsx
@@ -0,0 +1,12 @@
+/**
+ * Enum of flip types.
+ */
+export default {
+  LEFT: 'left',
+  RIGHT: 'right',
+  TOP_LEFT: 'top-left',
+  TOP_RIGHT: 'top-right',
+  BOTTOM_LEFT: 'bottom-left',
+  BOTTOM_RIGHT: 'bottom-right',
+  NONE: null
+};
