ff5daacfd88ffe6d55d03b346029ed1e3e9a01e1
gm <gleb.mihalkov.work@yandex.ru>
2018-08-08 14:48:16 +0300

    Adding AppContainer

---
+++ src/App/AppContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+
+export default ({ app }) => {
+  let node = (
+    <HashRouter>
+      <Route path="/" component={app} />
+    </HashRouter>
+  );
+
+  let rendered = new Promise(resolve => {
+    window.addEventListener('DOMContentLoaded', () => {
+      let root = document.getElementById('root');
+      render(node, root, resolve);
+    });
+  });
+
+  let loaded = new Promise(resolve => {
+    window.addEventListener('load', resolve, {once: true});
+  });
+
+  Promise.all([rendered, loaded]).then(() => {
+    let preload = document.getElementById('preload');
+    document.body.classList.remove('loading');
+    setTimeout(() => preload.remove(), 250);
+  });
+};
